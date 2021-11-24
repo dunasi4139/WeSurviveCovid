0af5f5a8775b92d261b19f77be79d047679622eb
idan <idantb3@gmail.com>
2021-11-24 13:57:23 +0700

    article page update and halaman vaksinasi

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <section class="page-title bg-1">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block text-center">
+                        <h1 class="text-capitalize mb-5 text-lg">Artikel</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="section blog-wrap pt-0">
         <div class="container">
             <div class="row">
