578698d09facd17ce67ec0163f022c314c8b763f
idan <idantb3@gmail.com>
2021-11-24 11:44:06 +0700

    article page (layout only)

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section blog-wrap pt-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-12 mb-5">
+                            <div class="single-blog-item"><img class="img-fluid" src="images/blog/blog-1.jpg" alt="" />
+                                <div class="blog-item-content mt-5">
+                                    <div class="blog-item-meta mb-3"><span class="text-color-2 text-capitalize mr-3"> Equipment</span> <span class="text-muted text-capitalize mr-3">5 Comments</span> <span class="text-black text-capitalize mr-3"> 28th January 2019</span></div>
+                                    <h2 class="mb-4 text-md"><a href="blog-single.html">Healthy environment to care with modern equipment</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus natus, consectetur? Illum libero vel nihil nisi quae, voluptatem, sapiente necessitatibus distinctio voluptates, iusto qui. Laboriosam autem, nam voluptate in beatae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae iure officia nihil nemo, repudiandae itaque similique praesentium non aut nesciunt facere nulla, sequi sunt nam temporibus atque earum, ratione, labore.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
