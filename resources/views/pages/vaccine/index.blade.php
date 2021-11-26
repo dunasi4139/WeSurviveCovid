708e05d06332f56f5f1cb7b3011a1f216d953c6f
Duwi Purnama Sidik <Duwi763@gmail.com>
2021-11-26 09:47:44 +0700

    Fix Vaksin

--- resources/views/pages/vaccine/index.blade.php
+++ resources/views/pages/vaccine/index.blade.php
@@ -4,53 +4,60 @@
 
 @section('content')
 
-<section class="page-title bg-1">
-  <div class="overlay"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="block text-center">
-          <h1 class="text-capitalize mb-5 text-lg">{{ trans('global.nav.vaccine') }}</h1>
-          <div class="card w-100 mb-3">
-            <div class="card-body" style="flex-direction: row">
-              <h5>Filter</h5>
-              <select class="form-control">
-                @foreach ($province as $p)
-                <option value="{{ $p->id }}">{{ $p->name }}</option>
-                @endforeach
-              </select>
+    <section class="page-title bg-1">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block text-center">
+                        <h1 class="text-capitalize mb-5 text-lg">{{ trans('global.nav.vaccine') }}</h1>
+                        <div class="card w-100 mb-3">
+                            <div class="card-body" style="flex-direction: row">
+                                <h5>Filter</h5>
+                                <form action="">
+                                    <select class="form-control" name="filter" id="filter" onchange="submit(filter)">
+                                        @foreach ($provinsi as $p)
+                                            <option value="{{ $p->id }}">{{ $p->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+    </section>
 
+    @foreach ($data as $d)
+        <section class="section doctor-single">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="doctor-img-block">
+                            <img src="images/team/1.jpg" alt="" class="img-fluid w-100">
+                        </div>
+                    </div>
 
-<section class="section doctor-single">
-  <div class="container">
-    <div class="row mb-5">
-      <div class="col-lg-4 col-md-6">
-        <div class="doctor-img-block">
-          <img src="images/team/1.jpg" alt="" class="img-fluid w-100">
-        </div>
-      </div>
-      @foreach($data as $d)
-      <div class="col-lg-8 col-md-6">
-        <div class="doctor-details mt-4 mt-lg-0">
-          <h2 class="text-md">{{ $d->nama }}</h2>
-          <div class="divider my-4"></div>
-          <p>Alamat : {{ $d->alamat }}</p>
-          <p>Jadwal : {{ $d->jadwal }}</p>
-          <p>Jenis : {{ $d->jenis }}</p>
-          <p>Persyaratan : {{ $d->syarat }}</p>
-        </div>
-      </div>
-      @endforeach
+                    <div class="col-lg-8 col-md-6">
+                        <div class="doctor-details mt-4 mt-lg-0">
+                            <h2 class="text-md">{{ $d->nama }}</h2>
+                            <div class="divider my-4"></div>
+                            <p>Alamat : {{ $d->alamat }}</p>
+                            <p>Jadwal : {{ $d->jadwal }}</p>
+                            <p>Jenis : {{ $d->jenis }}</p>
+                            <p>Persyaratan : {{ $d->syarat }}</p>
+                        </div>
+                    </div>
+                    {{-- {{ $data->links() }} --}}
+                </div>
+            </div>
+        </section>
+    @endforeach
 
-      {{-- {{ $data->links() }} --}}
-    </div>
-  </div>
-</section>
 @endsection
+
+<script>
+    function submit(id) {
+        document.getElementById(id).submit();
+    }
+</script>
